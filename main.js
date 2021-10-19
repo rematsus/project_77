c196b37e482a6d9ea6d4c38a24609486e40f352a
rematsus <92125753+rematsus@users.noreply.github.com>
2021-10-19 17:26:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("My first console project");
+
+var first_name="Victor";
+var last_name="Boruchowski";
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a=200;
+var b=50;
+var add=a+b;
+console.log(add);
+
+var subtract=a-b;
+console.log(subtract);
+
+var multiply=b*a;
+console.log(multiply);
+
+var divide=a/b;
+console.log(divide);
